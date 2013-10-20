0663e91fb99efadacb955e362616ddda903d04f9
VISTALL <vistall.valeriy@gmail.com>
2013-10-20 23:09:30 +0300

    corrected artifact add lib/ to python sdk classpath

--- google-py-app-engine-api/google-py-app-engine/src/org/consulo/google/appengine/py/sdk/PyAppSdkType.java
+++ google-py-app-engine-api/google-py-app-engine/src/org/consulo/google/appengine/py/sdk/PyAppSdkType.java
@@ -2,6 +2,10 @@ package org.consulo.google.appengine.py.sdk;
 
 import org.consulo.google.appengine.python.sdk.GoogleAppEnginePySdk;
 import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.projectRoots.SdkModificator;
+import com.intellij.openapi.roots.OrderRootType;
+import com.intellij.openapi.vfs.VirtualFile;
 
 /**
  * @author VISTALL
@@ -12,6 +16,34 @@ public class PyAppSdkType extends GoogleAppEnginePySdk
 	public PyAppSdkType()
 	{
 		super("GOOGLE_APP_ENGINE_PYTHON");
+	}
+
+	@Override
+	public void setupSdkPaths(Sdk sdk)
+	{
+		VirtualFile homeDirectory = sdk.getHomeDirectory();
+		if(homeDirectory == null)
+		{
+			return;
+		}
+
+		SdkModificator sdkModificator = sdk.getSdkModificator();
+
+		VirtualFile lib = homeDirectory.findChild("lib");
+		if(lib == null)
+		{
+			return;
+		}
+
+		for(VirtualFile virtualFile : lib.getChildren())
+		{
+			if(virtualFile.exists())
+			{
+				sdkModificator.addRoot(virtualFile, OrderRootType.CLASSES);
+			}
+		}
+
+		sdkModificator.commitChanges();
 	}
 
 	@NotNull
